7e152c59ae6029e419054f5e43fce94278b17018
Ozodbek Mirzohidov <ozodbek.mirzohidov@icloud.com>
2026-04-23 09:41:20 +0500

    feat: complete types

---
+++ types/index.ts
@@ -0,0 +1,50 @@
+export interface IProject {
+  id: string;
+  title: string;
+  subtitle: string | null;
+  image: string | null;
+  detail_link: string | null;
+  site_link: string | null;
+  order_index: number;
+  created_at: string;
+}
+
+export interface ISkill {
+  id: string;
+  name: string;
+  file: string;
+  ext: string;
+  order_index: number;
+  created_at: string;
+}
+
+export interface IExperience {
+  id: string;
+  date: string;
+  title: string;
+  company: string;
+  order_index: number;
+  created_at: string;
+}
+
+export interface IBlog {
+  id: string;
+  slug: string;
+  title: string;
+  image: string | null;
+  tag: string | null;
+  read_time: string | null;
+  content: string | null;
+  published_at: string;
+  created_at: string;
+}
+
+export interface IMessage {
+  id: string;
+  name: string;
+  email: string;
+  subject: string | null;
+  message: string;
+  is_read: boolean;
+  created_at: string;
+}
